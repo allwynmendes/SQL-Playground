5c98582f8cc973f8a5baa0907f821027e63df7db
Choco <allwyn.mendes@gmail.com>
2017-04-26 17:54:30 +0530

    Here : Local Procedure declaration, use of CASE in plsql, and define variables first and then define procedures below (Somewhat like C)

--- PLSQL/ssignment-04apr2017(PART-2).sql
+++ PLSQL/ssignment-04apr2017(PART-2).sql
@@ -1,21 +1,42 @@
+set serveroutput on;
+
+
 DECLARE
-	procedure cal_total_sal() is
+---Declare super(root) block variables first and then define procedures below the variables
+    ch number;
+	procedure cal_total_sal is
 	BEGIN
 		dbms_output.put_line('Entered cal_total_sal');
 	END cal_total_sal;
 	
-	procedure is
+	procedure cal_avg_sal is
 	BEGIN
-	END;
+		dbms_output.put_line('Entered cal_avg_sal');
+	END cal_avg_sal;
 	
-	procedure is
+	procedure cal_min_sal is
 	BEGIN
-	END;
+		dbms_output.put_line('Entered cal_min_sal');
+	END cal_min_sal;
 	
-	procedure is
+	procedure cal_max_sal is
 	BEGIN
-	END;
+		dbms_output.put_line('Entered cal_max_sal');
+	END cal_max_sal;
 	
 BEGIN
-
+	dbms_output.put_line('Select appropriate option : ');
+	dbms_output.put_line('TOTAL');
+	dbms_output.put_line('AVERAGE');
+	dbms_output.put_line('MIN');
+	dbms_output.put_line('MAX');
+	ch := &ch;
+	
+	case ch
+		when 1 then cal_total_sal;
+		when 2 then cal_avg_sal;
+		when 3 then cal_min_sal;
+		when 4 then cal_max_sal;
+	end case;
+	
 END;
