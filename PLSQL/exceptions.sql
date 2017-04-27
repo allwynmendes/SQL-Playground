0a9e7424055ef53b5bd04d0f46903e231befa257
Choco <allwyn.mendes@gmail.com>
2017-04-27 19:08:25 +0530

    Simple PLSQL code to try execptions

---
+++ PLSQL/exceptions.sql
@@ -0,0 +1,21 @@
+/*In PLSQL there are two classes of exceptions : 
+    1)Pre Defined
+    2)User Defined
+*/
+
+set serveroutput on;
+
+select * from employees;
+
+
+declare
+    ename employees.first_name%type;
+    esal employees.salary%type;
+begin
+        select first_name, salary into ename, esal from employees where employee_id = &empid;
+        dbms_output.put_line('Name   : '||ename);
+        dbms_output.put_line('Salary : '||esal);
+exception 
+    when others then
+        dbms_output.put_line('Error !!!');
+end;
