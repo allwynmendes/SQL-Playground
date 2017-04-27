971c0778d30075605bf4d38c48a70709c9cfbb6e
Choco <allwyn.mendes@gmail.com>
2017-04-27 19:29:23 +0530

    Code to RAISE Exceptions

--- PLSQL/exceptions.sql
+++ PLSQL/exceptions.sql
@@ -7,7 +7,7 @@ set serveroutput on;
 
 select * from employees;
 
-
+--Program to raise runtime exception NO_DATA_FOUND which is captured in exception block automatically
 declare
     ename employees.first_name%type;
     esal employees.salary%type;
@@ -21,3 +21,24 @@ exception
     when others then
         dbms_output.put_line('Error !!!');
 end;
+
+select * from employees;
+
+--Raising Exceptions explicitly using RAISE command (TAX Eligibility)
+DECLARE
+    tax_exp exception; --declare an expeption type
+    esal employees.salary%type;
+BEGIN
+    select salary into esal from employees where employee_id=&empid;
+    dbms_output.put_line('Salary : '||esal);
+    if esal > 10000 then
+        RAISE tax_exp;
+    else
+        dbms_output.put_line('Tax on Income : 0%');
+    end if;
+EXCEPTION
+    when tax_exp then
+        dbms_output.put_line('Error : Tax eligible on income.');
+    when others then
+        dbms_output.put_line('Error !!!');
+END;
