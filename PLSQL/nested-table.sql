9b7585bbfca586e00f72531b7e34c9dc1a39829a
Choco <allwyn.mendes@gmail.com>
2017-04-29 02:15:31 +0530

    Phew...gotta figure out how to effectively play with collections - Associative Arrays, Nested Tables, Varray

--- PLSQL/nested-table.sql
+++ PLSQL/nested-table.sql
@@ -7,8 +7,25 @@ DECLARE
     TYPE friends IS TABLE OF VARCHAR2(15);    
     names friends := friends('Prateek', 'Faizi', 'Jobin', 'Surya');
 BEGIN
+    names(4) := 'Rohan';
+    --names(5) := 'Surya';  --Does not work
     for i in names.first..names.last loop
+        dbms_output.put_line(i);
         dbms_output.put_line(names(i));
     end loop;
+END;
 
+--A slightly nore ambitious version of NESTED TABLES
+--Not Working (Gotta Figure out this Collections stuff)
+DECLARE
+    type rec is record(
+        name varchar2(15),
+        age number
+    );
+    r1 rec;
+    
+    type friends is table of r1;
+    names friends := friends();
+BEGIN
+    names.extend;
 END;
