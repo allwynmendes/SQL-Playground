9ad76b8179d9d453467ebc262a1d50628f06bf54
Choco <allwyn.mendes@gmail.com>
2017-05-04 13:48:38 +0530

    Applied Trigger on before update of emp table : Works like a boss

--- PLSQL/trigger1.sql
+++ PLSQL/trigger1.sql
@@ -1,5 +1,5 @@
 --Triggers
-
+set serveroutput on;
 select * from emp;
 
 create table emp(
@@ -22,9 +22,34 @@ insert into emp values(s1.nextval, 'C', 'Tester', 7500);
 insert into emp values(s1.nextval, 'D', 'Database', 7750);
 insert into emp values(s1.nextval, 'E', 'Dev-Ops', 6000);
 
-create table emp_dump(
+--Here, Creating triggers and tables to dump triggered values into
+
+create table emp_update_dump(
     id references emp(id),
     ename varchar(20),
+    old_sal number(7,2),
+    new_sal number(7,2),
     user1 varchar(20),
     date1 date
 );
+
+create or replace trigger emp_tr1
+before update on emp
+for each row
+DECLARE
+    sal_diff number(7,2);
+BEGIN
+    sal_diff := :new.esal - :old.esal;
+    dbms_output.put_line('Old Salary : '||:old.esal);
+    dbms_output.put_line('New Salary : '||:new.esal);
+    dbms_output.put_line('Salary Diff : '||sal_diff);
+    if (:old.esal < :new.esal) then
+        dbms_output.put_line('Salary Increase');
+    else
+        dbms_output.put_line('Salary Decrease');
+    end if;
+END;
+
+show error;
+
+update emp set esal=15000 where ejob = 'CEO';
