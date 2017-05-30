a12742044e8e7efa7b518eb3f840ca1bbfe7a973
allwynmendes <allwyn.mendes@gmail.com>
2017-05-30 18:06:20 +0530

    Successfully retrieved foreign key linked tables to the original table in which there is primary key.

--- rpro-1.sql
+++ rpro-1.sql
@@ -22,7 +22,26 @@ select * from all_constraints;
 select * from all_cons_columns;
 
 /*--------------------------------------------------------------------*/
-select * from all_constraints WHERE constraint_type='R' AND CONSTRAINT_NAME='RPRO_USERS_R01';
 
+select * from all_constraints WHERE constraint_type='R' and table_name like 'RPRO_%';
+
+SELECT * FROM RPRO_DATASET_FIELD_DEF;
+desc RPRO_DATASET_FIELD_DEF;
+
+select dbms_metadata.get_ddl( 'TABLE', 'RPRO_DATASET_FIELD_DEF', 'REVPRO_RGURU_NEW' ) from dual;
+
+select * from RPRO_DATASET_DEFINITIONS;
+desc RPRO_DATASET_DEFINITIONS;
+
+select table_name from user_constraints
+where r_constraint_name in
+  (select constraint_name 
+     from user_constraints
+     where constraint_type in ('P','U')
+     and table_name = upper('RPRO_DATASET_DEFINITIONS')
+  );
+
+select * from all_constraints where table_name=upper('RPRO_DATASET_DEFINITIONS');
+select * from all_cons_columns where table_name=upper('RPRO_DATASET_DEFINITIONS');
 
 
