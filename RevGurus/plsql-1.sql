3a0006c2e463d6403718cb75a5234eeb47fdd109
allwynmendes <allwyn.mendes@gmail.com>
2017-06-01 12:02:14 +0530

    Described the tables being used for the PLSQL demo

--- RevGurus/plsql-1.sql
+++ RevGurus/plsql-1.sql
@@ -8,3 +8,16 @@ EXCEPTION (optiona;)
 
 END;    (mandatory)
 */
+
+/*
+Create and Insert script that will be considered for the folling plsql-x.sql files is linked at 
+"https://livesql.oracle.com/apex/livesql/file/content_O5AEB2HE08PYEPTGCFLZU9YCV.html"
+*/
+
+select * from tab;
+/*
+Output : 
+TNAME	TABTYPE	CLUSTERID
+DEPT	TABLE	 - 
+EMP	TABLE	 - 
+*/
