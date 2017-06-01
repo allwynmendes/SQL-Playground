f8312a7ed3fe02cbf165a1d13523924878228ba5
allwynmendes <allwyn.mendes@gmail.com>
2017-06-01 13:22:44 +0530

    Implemented Code Below using <<labels>>, Errors in code are expected

--- RevGurus/plsql-4.sql
+++ RevGurus/plsql-4.sql
@@ -1,7 +1,6 @@
 --Blocks and subblocks
 
 --implementing subblock within a block
-
 declare
    var1 number:= 10;
    
@@ -20,8 +19,29 @@ begin
        dbms_output.put_line(var1); 
 end;
 
-
 /*output:
 ORA-06550: line 15, column 29:
 PLS-00201: identifier 'VAR2' must be declared 
+*/
+/*--------------------------------------------------------------------------------------------------------*/
+
+--Implemented Code Below using <<labels>>
+declare
+    var1 number:= 10;
+begin
+    dbms_output.put_line(var1);
+    <<B1>>
+    declare
+        var1 number:=20;
+    begin
+        dbms_output.put_line(var1);
+        dbms_output.put_line(B1.var1);
+    end;
+    dbms_output.put_line(var1);
+    dbms_output.put_line(B1.var1); 
+end;
+
+/*
+ORA-06550: line 16, column 32:
+PLS-00219: label 'B1' reference is out of scope 
 */
