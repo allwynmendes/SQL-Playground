cef4a071166f605b6a12f15baac1c0df2ec9522c
allwynmendes <allwyn.mendes@gmail.com>
2017-06-01 13:11:47 +0530

    Create plsql-4.sql

---
+++ RevGurus/plsql-4.sql
@@ -0,0 +1,27 @@
+--Blocks and subblocks
+
+--implementing subblock within a block
+
+declare
+   var1 number:= 10;
+   
+begin
+     dbms_output.put_line(var1);
+     
+     declare
+         var2 number:=20;
+         
+         begin
+            dbms_output.put_line(var2);
+            dbms_output.put_line(var1);
+        end;
+       
+       dbms_output.put_line(var2);--line15
+       dbms_output.put_line(var1); 
+end;
+
+
+/*output:
+ORA-06550: line 15, column 29:
+PLS-00201: identifier 'VAR2' must be declared 
+*/
