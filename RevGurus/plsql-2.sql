181bbe0393667e336af72ca592463194a1fc6f30
allwynmendes <allwyn.mendes@gmail.com>
2017-06-01 12:32:05 +0530

    Minor changes made, also implemented %rowtype

--- RevGurus/plsql-2.sql
+++ RevGurus/plsql-2.sql
@@ -13,7 +13,20 @@ END;
 --The same using %type
 DECLARE 
     var1 emp.ename%type;    --use of %type
+    var2 emp.sal%type;
 BEGIN
-    select ename into var1 from emp where job = 'PRESIDENT';
+    select ename, sal into var1, var2 from emp where job = 'PRESIDENT';
     dbms_output.put_line('H1 '||var1||' !');
+    dbms_output.put_line('Your salary is '||var2);
+END;
+
+--Similar thing using %rowtype
+DECLARE 
+    var1 emp%rowtype;
+BEGIN
+    select * into var1 from emp where job = 'PRESIDENT';
+    dbms_output.put_line('EMPNO : '||var1.empno);
+    dbms_output.put_line('NAME  : '||var1.ename);
+    dbms_output.put_line('JOB   : '||var1.job);
+    dbms_output.put_line('SAL   : '||var1.sal);
 END;
