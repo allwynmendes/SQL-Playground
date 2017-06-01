afb30b96fd0a86171002f8edfdf02f4fcd5594e3
allwynmendes <allwyn.mendes@gmail.com>
2017-06-01 12:19:12 +0530

    Initiated plsql-2.sql

---
+++ RevGurus/plsql-2.sql
@@ -0,0 +1,11 @@
+/*
+Code below demonstrates the use of select into in PLSQL.
+And says Hi to the President of the organization
+*/
+
+DECLARE 
+    var1 varchar2(10);
+BEGIN
+    select ename into var1 from emp where job = 'PRESIDENT';
+    dbms_output.put_line('H1 '||var1||' !');
+END;
