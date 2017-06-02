5d128bb4ffe2f5167376d9402c969a6070fe8c1f
allwynmendes <allwyn.mendes@gmail.com>
2017-06-02 12:51:29 +0530

    Created state table

--- Bus_Reservation/db_schema1.sql
+++ Bus_Reservation/db_schema1.sql
@@ -1,1 +1,24 @@
 --This file contains schema for bus reservation system
+
+--Tables related to locationing for buses (state-location-sublocation)
+create table states(
+    state_id varchar2(2),
+    state_name varchar2(30),
+    primary key(state_id)
+);
+
+create table location(
+    location_id number(3),
+    location_name varchar2(30),
+    state_id varchar2(2),
+    primary key(location_id),
+    foreign key(state_id) references states(state_id)
+);
+
+create table sub_location(
+    slocation_id number(4),
+    slocation_name varchar2(30),
+    location_id number(3),
+    primary key(slocation_id),
+    foreign key(location_id) references location(location_id)
+);
