38c27ad5866c77b2569c735479bbce45f01c10ad
allwynmendes <allwyn.mendes@gmail.com>
2017-06-05 16:18:28 +0530

    Data added for bus_company and bus_owners

--- Bus_Reservation/bus_data.sql
+++ Bus_Reservation/bus_data.sql
@@ -2,3 +2,13 @@
 insert into buses values('MH01AK47',40,'1','SEATER');
 insert into buses values('MH12DB1112',40,'0','SLEEPER');
 insert into buses values('GA03AE4318',30,'1','SEMI-SLEEPER');
+
+--Bus_Company
+insert into bus_company values(1,'NEETA','neeta123');
+insert into bus_company values(2,'VRL','vrl123');
+insert into bus_company values(3,'KONDUSKAR','kondu123');
+
+--Bus_owners
+insert into bus_owners values(1,'MH12DB1112');
+insert into bus_owners values(1,'MH01AK47');
+insert into bus_owners values(2,'GA03AE4318');
