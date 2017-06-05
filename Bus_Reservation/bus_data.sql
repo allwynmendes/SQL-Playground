9d819e9b4f884e7e8dfbd24dd4cecee926b19521
allwynmendes <allwyn.mendes@gmail.com>
2017-06-05 16:27:39 +0530

    values inserted for buslocation table

--- Bus_Reservation/bus_data.sql
+++ Bus_Reservation/bus_data.sql
@@ -12,3 +12,8 @@ insert into bus_company values(3,'KONDUSKAR','kondu123');
 insert into bus_owners values(1,'MH12DB1112');
 insert into bus_owners values(1,'MH01AK47');
 insert into bus_owners values(2,'GA03AE4318');
+
+--Bus_location
+insert into bus_location values('MH12DB1112', '18.496753, 73.857216', sysdate);
+insert into bus_location values('MH01AK47', '19.110482, 72.872888', sysdate);
+insert into bus_location values('GA03AE4318','15.906560, 73.823572',sysdate-1);
