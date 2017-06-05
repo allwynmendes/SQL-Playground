483a5c75ad3fc33896801cef35e7246eb5bead7a
allwynmendes <allwyn.mendes@gmail.com>
2017-06-05 15:21:50 +0530

    Converted number to varchar2(2)

--- Bus_Reservation/location_data.sql
+++ Bus_Reservation/location_data.sql
@@ -4,14 +4,17 @@ insert into states values('GA', 'Goa');
 insert into states values('KA', 'Karnataka');
 
 --Location
-insert into location values(12, 'MH', 'Pune');
-insert into location values(10,  'MH', 'Sangli');
-insert into location values(01, 'MH' ,'Mumbai');
+insert into location values('12', 'MH', 'Pune');
+insert into location values('10',  'MH', 'Sangli');
+insert into location values('01', 'MH' ,'Mumbai');
 
-insert into location values(03, 'GA', 'Mapusa');
-insert into location values(05, 'GA', 'Panjim');
-insert into location values(07, 'GA', 'Margaon');
+insert into location values('03', 'GA', 'Mapusa');
+insert into location values('05', 'GA', 'Panjim');
+insert into location values('07', 'GA', 'Margaon');
 
-insert into location values(01, 'KA', 'Bangalore');
-insert into location values(22, 'KA', 'Belgaum');
-insert into location values(09, 'KA', 'Mysore');
+insert into location values('01', 'KA', 'Bangalore');
+insert into location values('22', 'KA', 'Belgaum');
+insert into location values('09', 'KA', 'Mysore');
+
+--Sub_location
+
