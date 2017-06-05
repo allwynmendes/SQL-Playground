c5316722edd1e515666540d635553a1fa5fe0c02
allwynmendes <allwyn.mendes@gmail.com>
2017-06-05 16:09:53 +0530

    data inserted into buses table

---
+++ Bus_Reservation/bus_data.sql
@@ -0,0 +1,4 @@
+--Buses
+insert into buses values('MH01AK47',40,'1','SEATER');
+insert into buses values('MH12DB1112',40,'0','SLEEPER');
+insert into buses values('GA03AE4318',30,'1','SEMI-SLEEPER');
