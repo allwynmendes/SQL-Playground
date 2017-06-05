eb4fd354a24739fbad12767ec2e2f4faaeb198f4
allwynmendes <allwyn.mendes@gmail.com>
2017-06-05 15:12:09 +0530

    Added location data

--- Bus_Reservation/location_data.sql
+++ Bus_Reservation/location_data.sql
@@ -2,3 +2,16 @@
 insert into states values('MH', 'Maharashtra');
 insert into states values('GA', 'Goa');
 insert into states values('KA', 'Karnataka');
+
+--Location
+insert into location values(12, 'MH', 'Pune');
+insert into location values(10,  'MH', 'Sangli');
+insert into location values(01, 'MH' ,'Mumbai');
+
+insert into location values(03, 'GA', 'Mapusa');
+insert into location values(05, 'GA', 'Panjim');
+insert into location values(07, 'GA', 'Margaon');
+
+insert into location values(01, 'KA', 'Bangalore');
+insert into location values(22, 'KA', 'Belgaum');
+insert into location values(09, 'KA', 'Mysore');
