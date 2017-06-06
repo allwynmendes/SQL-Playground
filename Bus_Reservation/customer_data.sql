7bcdfc3c8becf10eceafcd5f191d5f73f15a842c
allwynmendes <allwyn.mendes@gmail.com>
2017-06-06 11:31:57 +0530

    Created 2 customers

---
+++ Bus_Reservation/customer_data.sql
@@ -0,0 +1,7 @@
+--Customers
+insert into customer values(1, 'Shiwani', '8109773714', 'agarwalshiwani301@gmail.com', 'F', 25);
+insert into customer values(2, 'Priya', '9702499793', 'priya.tawar25@gmail.com', 'F', 25);
+
+--Customer_login
+insert into customer_login values(1, 'shiwani', 'shiwani123');
+insert into customer_login values(2, 'priya', 'priya123'); 
