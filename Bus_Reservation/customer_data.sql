57dbccab0ca3f3f391f3b8b1fd12d8b8d9321d78
allwynmendes <allwyn.mendes@gmail.com>
2017-06-06 11:34:49 +0530

    Added information into ticket and passengers table

--- Bus_Reservation/customer_data.sql
+++ Bus_Reservation/customer_data.sql
@@ -5,3 +5,10 @@ insert into customer values(2, 'Priya', '9702499793', 'priya.tawar25@gmail.com',
 --Customer_login
 insert into customer_login values(1, 'shiwani', 'shiwani123');
 insert into customer_login values(2, 'priya', 'priya123'); 
+
+--Ticket
+insert into ticket values(1, 1, 2, 3, 5, 400, 2);
+
+--Passengers
+insert into passengers values(1, 'Priya', 'F', 25, 5);
+insert into passengers values(1, 'Shiwani', 'F', 25, 6);
