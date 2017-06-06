78d10c485fb2eb251799339b7a9f03881987120b
allwynmendes <allwyn.mendes@gmail.com>
2017-06-06 12:33:02 +0530

    Trigger fired on ticket table

--- Bus_Reservation/customer_data.sql
+++ Bus_Reservation/customer_data.sql
@@ -4,7 +4,26 @@ insert into customer values(2, 'Priya', '9702499793', 'priya.tawar25@gmail.com',
 
 --Customer_login
 insert into customer_login values(1, 'shiwani', 'shiwani123');
-insert into customer_login values(2, 'priya', 'priya123'); 
+insert into customer_login values(2, 'priya', 'priya123');
+
+/*------------------------TRIGGER----------------------------*/
+/*
+This trigger updates the number of seats in bus_trip table
+based on inserts into ticket table
+*/
+create or replace trigger update_seats
+after insert on ticket
+for each row
+declare
+    tripid number;
+    seats_avail number;
+begin
+    update bus_trip set seats_available = seats_available - :new.no_of_tickets;
+    select trip_id, seats_available into tripid, seats_avail from bus_trip;
+    dbms_output.put_line('Trip Id          : '||tripid);
+    dbms_output.put_line('Seates Available : '||seats_avail);
+end;
+/*------------------------END--------------------------------*/
 
 --Ticket
 insert into ticket values(1, 1, 2, 3, 5, 400, 2);
