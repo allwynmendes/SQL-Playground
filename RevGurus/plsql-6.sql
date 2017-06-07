61943dff29c56ab0efa422c60b429687734e8622
allwynmendes <allwyn.mendes@gmail.com>
2017-06-07 12:40:14 +0530

    finding no is palindrome or not

--- RevGurus/plsql-6.sql
+++ RevGurus/plsql-6.sql
@@ -11,3 +11,22 @@ BEGIN
     end loop;
     dbms_output.put_line(x);
 END;
+
+--Palindrome number
+DECLARE
+    n number:=123;
+    r number;
+    x number:=0;
+BEGIN
+    while n > 0 loop
+        r:= mod(n,10);
+        x := (x*10) + r;
+        n := trunc(n/10);
+    end loop;
+    dbms_output.put_line(x);
+        if x=n then 
+                dbms_output.put_line('palindrome');
+            else
+                dbms_output.put_line('not palindrome');
+        end if;        
+END;
