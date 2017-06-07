b572151057289c10e4384426271dbae7f9c79b64
allwynmendes <allwyn.mendes@gmail.com>
2017-06-07 13:43:58 +0530

    armstrong number

--- RevGurus/plsql-6.sql
+++ RevGurus/plsql-6.sql
@@ -30,3 +30,27 @@ BEGIN
                 dbms_output.put_line('not palindrome');
         end if;        
 END;
+
+--Armstrong number
+DECLARE 
+    original_num number;
+    r number;
+    sum1 number := 0;
+    len1 number;
+    num1 number;
+BEGIN
+    for n in 1..100000 loop
+        original_num := n;
+        num1 := n;
+        select length(original_num) into len1 from dual;
+        while num1 > 0 loop
+            r := mod(num1, 10);           
+            sum1 := sum1 + power(r,len1);
+            num1 := trunc(num1/10);
+        end loop;
+        if sum1 = original_num then
+            dbms_output.put_line(original_num);
+        end if;
+        sum1 := 0;
+    end loop;
+END;
