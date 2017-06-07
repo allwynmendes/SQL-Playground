929afa19e1dfbe5002b434b29144d52c279acbd5
allwynmendes <allwyn.mendes@gmail.com>
2017-06-07 11:33:43 +0530

    If-else block

--- RevGurus/plsql-5.sql
+++ RevGurus/plsql-5.sql
@@ -2,3 +2,14 @@
   1)Un-Conditional
   2)Conditional
 */
+--simple if-else block
+declare
+    x number:=10;
+begin
+    x:=x+10;
+    if x > 50 then
+      dbms_output.put_line(x);
+    else
+      dbms_output.put_line('exit');
+    end  if;
+end;
